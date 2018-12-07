e7277a22f9f8ad8bdd2ae241d870de19b25a1a13
lusdavtyan <43096594+lusdavtyan@users.noreply.github.com>
2018-12-07 17:01:26 +0400

    Add files via upload

---
+++ question.js
@@ -0,0 +1,37 @@
+var questions= [
+{	"question": "Who r u",
+	"option1": "person",
+	"option2": "human",
+	"option3": "monkey",
+	"option4": "student",
+	"answer": "3"
+},
+{	"question": "heyoo",
+	"option1": "lol",
+	"option2": "cool",
+	"option3": "joyy",
+	"option4": "wtf",
+	"answer": "2"
+},
+{	"question": "what's ur name",
+	"option1": "don't know",
+	"option2": "r u kiddin me?",
+	"option3": "want another question",
+	"option4": "i don't know",
+	"answer": "4"
+},
+{	"question": "ok google",
+	"option1": "u r right",
+	"option2": "where r u?",
+	"option3": "u make me mad",
+	"option4": "pls finish this quickly",
+	"answer": "4"
+},
+{	"question": "last but not least?",
+	"option1": "finally",
+	"option2": "finallllllyyyyyyy",
+	"option3": "i want more",
+	"option4": "babye",
+	"answer": "2"
+}
+]
